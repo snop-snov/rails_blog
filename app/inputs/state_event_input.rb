30308330525796e32fd16269dcb1203590906aa7
snop_snov <ssaweleva@gmail.com>
2013-08-02 11:12:16 +0000

    add state_event_input

---
+++ app/inputs/state_event_input.rb
@@ -0,0 +1,32 @@
+class StateEventInput < SimpleForm::Inputs::CollectionSelectInput
+  def collection
+    object.send(state_transitions_name)
+  end
+
+  def input
+    label_method = :human_to_name
+    value_method = :event
+    input_options.merge!({ :include_blank => object.send(human_state_name) })
+
+    out = @builder.collection_select(
+      attribute_name, collection, value_method, label_method,
+      input_options, input_html_options
+    )
+  end
+
+  private
+
+  def state_machine_name
+    state_machine_name = attribute_name.to_s.split('_')
+    state_machine_name.delete 'event'
+    state_machine_name.join '_'
+  end
+
+  def state_transitions_name
+    "#{state_machine_name}_transitions"
+  end
+
+  def human_state_name
+    "human_#{state_machine_name}_name"
+  end
+end
