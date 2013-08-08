c13de358064987848560f62a858cfa14a687c47c
snop_snov <ssaweleva@gmail.com>
2013-08-08 07:11:07 +0000

    add locale

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ load 'config/configus.rb'
 module Blog
   class Application < Rails::Application
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.i18n.default_locale = :ru
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
