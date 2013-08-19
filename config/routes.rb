43c807d7735fe5360e3ef10962a87811d8a30775
snop_snov <ssaweleva@gmail.com>
2013-08-19 07:15:14 +0000

    post -> posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Blog::Application.routes.draw do
     # The priority is based upon order of creation: first created -> highest priority.
     # See how all your routes lay out with "rake routes".
     resources :posts do
-      scope module: :post do
+      scope module: :posts do
         resources :comments
       end
     end
